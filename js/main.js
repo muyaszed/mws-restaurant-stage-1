ebdbf7554abd098c41b31842e1d901bbc043e42b
Yazed Jamal <muyaszed@gmail.com>
2018-04-02 10:55:53 +0100

    fix: added alt to images

--- js/main.js
+++ js/main.js
@@ -154,6 +154,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `${restaurant.name}'s restaurant picture`;
   picture.append(image);
 
   const name = document.createElement('h1');
